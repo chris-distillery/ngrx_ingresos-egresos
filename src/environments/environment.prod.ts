6f9a6dc30d21c3c1213099b9efaeb89d247aa336
Christian Algarin <christian.algarin@distillery.com>
2023-07-25 02:27:54 -0300

    adding firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCRB-fndQOyMMUTtqqLZUAW05Jj40HxiNo",
+    authDomain: "ingreso-egreso-app-a62ae.firebaseapp.com",
+    projectId: "ingreso-egreso-app-a62ae",
+    storageBucket: "ingreso-egreso-app-a62ae.appspot.com",
+    messagingSenderId: "596277445169",
+    appId: "1:596277445169:web:9c23cae6ba31e618decb10",
+
+    databaseURL: "https://ingreso-egreso-app.firebaseio.com",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCRB-fndQOyMMUTtqqLZUAW05Jj40HxiNo",
+    authDomain: "ingreso-egreso-app-a62ae.firebaseapp.com",
+    projectId: "ingreso-egreso-app-a62ae",
+    storageBucket: "ingreso-egreso-app-a62ae.appspot.com",
+    messagingSenderId: "596277445169",
+    appId: "1:596277445169:web:9c23cae6ba31e618decb10",
+
+    databaseURL: "https://ingreso-egreso-app.firebaseio.com",
+  },
 };
 
 /*
